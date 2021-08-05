ea9731008707b20c8a4c1fdda2b547e6593c1951
Derenik <deren.kha@gmail.com>
2021-08-06 02:12:25 +0400

    source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
 module.exports = {
-  mode: "development",
+  mode,
 
   module: {
-      rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-          }
-      ]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
 
-  devtool: false,
+  devtool: "source-map",
   devServer: {
     contentBase: "./dist",
   },
