052276e6a7522c06da4dbca1bd7487a5cb3f1d80
Derenik <deren.kha@gmail.com>
2021-08-06 01:43:25 +0400

    webpack init with basics

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const ucraftMainTeams = {
+  hoory: "good team",
+  builder: "cool team",
+  ucCommerce: "best team :D",
+};
+
+const ucraftAllTeams = {
+  ...ucraftMainTeams,
+  springBuilder: "really big team",
+  aiTeam: "Levon is master jedi",
+};
+
+console.log(ucraftMainTeams);
+console.log(ucraftAllTeams);
